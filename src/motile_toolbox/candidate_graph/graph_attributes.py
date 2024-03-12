bcaabe3c120a5203c4b2ca6a904041518c2e7bff
Caroline Malin-Mayor <malinmayorc@janelia.hhmi.org>
2024-03-11 20:09:19 -0400

    Enumerate node and edge attributes

---
+++ src/motile_toolbox/candidate_graph/graph_attributes.py
@@ -0,0 +1,19 @@
+from enum import Enum
+
+
+class NodeAttr(Enum):
+    """Node attributes that can be added to candidate graph using the toolbox.
+    Note: Motile can flexibly support any custom attributes. The toolbox provides
+    implementations of commonly used ones, listed here.
+    """
+
+    SEG_ID = "segmentation_id"
+
+
+class EdgeAttr(Enum):
+    """Edge attributes that can be added to candidate graph using the toolbox.
+    Note: Motile can flexibly support any custom attributes. The toolbox provides
+    implementations of commonly used ones, listed here.
+    """
+
+    DISTANCE = "distance"
