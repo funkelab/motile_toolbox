257d7cbc1f1aa1f9e7a16d3164d8e6d430fde746
Caroline Malin-Mayor <malinmayorc@janelia.hhmi.org>
2024-05-22 21:09:04 -0400

    Automatic versioning with setuptools_scm

--- src/motile_toolbox/__init__.py
+++ src/motile_toolbox/__init__.py
@@ -1,3 +1,10 @@
 """
 .. include:: ../../README.md
 """
+from importlib.metadata import PackageNotFoundError, version
+
+try:
+    __version__ = version("motile-toolbox")
+except PackageNotFoundError:
+    # package is not installed
+    __version__ = "uninstalled"
