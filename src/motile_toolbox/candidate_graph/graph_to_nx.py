f61f0550b85a0f30ecfa6a177c70e073950d893d
Caroline Malin-Mayor <malinmayorc@janelia.hhmi.org>
2024-08-08 15:33:52 -0400

    Add hyperedges to nx graph function

--- src/motile_toolbox/candidate_graph/graph_to_nx.py
+++ src/motile_toolbox/candidate_graph/graph_to_nx.py
@@ -3,7 +3,9 @@ from motile import TrackGraph
 
 
 def graph_to_nx(graph: TrackGraph) -> nx.DiGraph:
-    """Convert a motile TrackGraph into a networkx DiGraph.
+    """Convert a motile TrackGraph into a networkx DiGraph. Hyper edges
+    are "flattened" into individual edges and the attributes from the hyper edge
+    are put on all individual edges.
 
     Args:
         graph (TrackGraph): TrackGraph to be converted to networkx
@@ -14,8 +16,13 @@ def graph_to_nx(graph: TrackGraph) -> nx.DiGraph:
     nx_graph = nx.DiGraph()
     nodes_list = list(graph.nodes.items())
     nx_graph.add_nodes_from(nodes_list)
-    edges_list = [
-        (edge_id[0], edge_id[1], data) for edge_id, data in graph.edges.items()
-    ]
+    edges_list = []
+    for edge_id, data in graph.edges.items():
+        if graph.is_hyperedge(edge_id):
+            for source in edge_id[0]:
+                for target in edge_id[1]:
+                    edges_list.append((source, target, data))
+        else:
+            edges_list.append((edge_id[0], edge_id[1], data))
     nx_graph.add_edges_from(edges_list)
     return nx_graph
