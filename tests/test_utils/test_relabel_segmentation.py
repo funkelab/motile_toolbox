94aaaa5c101b0dc3d15946acf072e073b6ca37e4
Caroline Malin-Mayor <malinmayorc@janelia.hhmi.org>
2024-11-26 14:12:23 -0500

    Add function to ensure unique label ids across time and hypothesis

--- tests/test_utils/test_relabel_segmentation.py
+++ tests/test_utils/test_relabel_segmentation.py
@@ -1,5 +1,8 @@
 import numpy as np
-from motile_toolbox.utils import relabel_segmentation
+from motile_toolbox.utils import (
+    ensure_unique_labels,
+    relabel_segmentation_with_track_id,
+)
 from numpy.testing import assert_array_equal
 from skimage.draw import disk
 
@@ -16,10 +19,43 @@ def test_relabel_segmentation(segmentation_2d, graph_2d):
     expected[1][rr, cc] = 1
 
     graph_2d.remove_node("1_2")
-    relabeled_seg = relabel_segmentation(graph_2d, segmentation_2d)
+    relabeled_seg = relabel_segmentation_with_track_id(graph_2d, segmentation_2d)
     print(f"Nonzero relabeled: {np.count_nonzero(relabeled_seg)}")
     print(f"Nonzero expected: {np.count_nonzero(expected)}")
     print(f"Max relabeled: {np.max(relabeled_seg)}")
     print(f"Max expected: {np.max(expected)}")
 
     assert_array_equal(relabeled_seg, expected)
+
+
+def test_ensure_unique_labels_2d(segmentation_2d):
+    expected = segmentation_2d.copy().astype(np.uint64)
+    frame = expected[1]
+    frame[frame == 2] = 3
+    frame[frame == 1] = 2
+    expected[1] = frame
+
+    print(np.unique(expected[1], return_counts=True))
+    result = ensure_unique_labels(segmentation_2d)
+    assert_array_equal(expected, result)
+
+
+def test_ensure_unique_labels_2d_multiseg(multi_hypothesis_segmentation_2d):
+    expected = multi_hypothesis_segmentation_2d.copy().astype(np.uint64)
+
+    # add 1 to the first hypothesis second frame
+    h0f1 = expected[0, 1]
+    h0f1[h0f1 == 2] = 3
+    h0f1[h0f1 == 1] = 2
+    expected[0, 1] = h0f1
+    # add 3 to the second hypothesis first frame
+    h1f0 = expected[1, 0]
+    h1f0[h1f0 == 1] = 4
+    expected[1, 0] = h1f0
+    # add 4 to the second hypothesis second frame
+    h1f1 = expected[1, 1]
+    h1f1[h1f1 == 1] = 5
+    h1f1[h1f1 == 2] = 6
+    expected[1, 1] = h1f1
+    result = ensure_unique_labels(multi_hypothesis_segmentation_2d, multiseg=True)
+    assert_array_equal(expected, result)
